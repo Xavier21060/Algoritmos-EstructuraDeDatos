7e98751dae0a54e435f0e7624be640fffdda37d9
Xavier210602 <xaviersan2106@gmail.com>
2023-02-13 22:42:37 -0500

    MAp para contar palabras

--- Algoritmos/src/main/java/com/estructuradatos/algoritmos/map/ContarOfWords.java
+++ Algoritmos/src/main/java/com/estructuradatos/algoritmos/map/ContarOfWords.java
@@ -1,7 +1,64 @@
 package com.estructuradatos.algoritmos.map;
 
+import java.util.*;
+
 public class ContarOfWords {
 
 
+    public static void main(String[] args) {
+
+        Map<String, Integer> miMap = new HashMap<String, Integer>();
+        createMap(miMap);
+
+        showMap(miMap);
+
+    }
+
+    public static void createMap(Map<String, Integer> map) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriba una Cadena: ");
+        String entrada = scanner.nextLine();
+
+        String [] tokens = entrada.split (" ");
+
+        for (String token : tokens) {
+
+            String words = token.toLowerCase();
+
+            if(map.containsKey(words)) {
+
+                int cuenta = map.get(words); //Obtiene la cuenta
+                map.put(words, cuenta);
+
+            }else {
+                map.put(words, 1);
+            }
+
+        }
+
+    }
+
+    public static void showMap(Map<String, Integer> map) {
+
+        Set<String> keys = map.keySet();
+
+        TreeSet<String> orderKeys = new TreeSet<>(keys);
+
+        System.out.println(
+                "\nEl mapa contienes: %nClave\t\tValor%n"
+        );
+
+        for (String clave: orderKeys){
+            System.out.printf("%-10s%10s%n", clave, map.get(clave));
+
+        }
+
+        System.out.printf("Tamaño: %d%n¿vacio?: %b%n", map.size(), map.isEmpty());
+
+
+
+
+    }
 
 }
