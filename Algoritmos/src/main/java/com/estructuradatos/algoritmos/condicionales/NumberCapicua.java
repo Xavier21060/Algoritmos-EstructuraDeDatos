dcadf59106f578814ed5071fe6d7309c8388c8df
Xavier21060 <xaviersan2106@gmail.com>
2023-03-04 16:30:41 -0500

    Se determina si un numero es capicua

---
+++ Algoritmos/src/main/java/com/estructuradatos/algoritmos/condicionales/NumberCapicua.java
@@ -0,0 +1,45 @@
+package com.estructuradatos.algoritmos.condicionales;
+
+import java.util.Scanner;
+
+/** 
+ * Escribe un programa que diga si un numero positivo introducido por teclado es capicua
+ * @author usuario
+ *
+ */
+public class NumberCapicua {
+
+	private static Scanner sc;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite un numero: ");
+		int n = sc.nextInt();
+		
+		System.out.println(isCapicua(n));
+		
+	}
+	
+	public static boolean isCapicua(int n) {
+		
+		String numString = String.valueOf(n);
+		
+	
+		for(int i = 0, j = numString.length()-1; j <=i; i++, --j) {
+			
+			if (numString.charAt(i) != numString.charAt(j)) {
+				return false;
+			}
+			
+		}
+	
+		
+		return true;
+		
+	}
+	
+	
+	 
+	
+}
